0adb58c11980d9c39012a265b91fae93fce0d0ee
Uddhav Agarwal <leouddhav@gmail.com>
2019-04-25 16:36:14 +0100

    working SAT encoder

--- fomin.py
+++ fomin.py
@@ -8,19 +8,22 @@ Created on Fri Apr 19 13:07:09 2019
 
 import networkx as nx 
 import math
-import copy
+import copy   
+import random    
+import glob
+import signal
+from networkx.algorithms.approximation import treewidth
+import itertools
+import time
+import os
 
-def tdAux(S):
-    depth = 0
-    
-    if len(S) == 1:
-        depth = 1
-    else:
-        S.pop()
-        depth = 1 + tdAux(copy.deepcopy(S))
-    
-    return depth 
-        
+class TimeoutError(RuntimeError):
+    pass
+
+def handler(signum, frame):
+    print("Timeout exception")
+#    raise Exception("end of time!!")
+    raise TimeoutError()
         
 def td(G):
     
@@ -29,105 +32,256 @@ def td(G):
 
     if (len(copG.nodes) == 1):
         depth = 1
-        print('case1')
+#        print('case1')
 #        return depth
         
     elif (len(copG.nodes) > 1 and nx.is_connected(copG)):
         someList = []
-        print('case2')
+#        print('case2')
 
         for v in list(copG.nodes)[:-1]:
             edges = list(copG.edges(v))
             copG.remove_node(v)            
             someList.append(td(copG))
-#            print(copG.nodes)
             copG.add_node(v)
-#            print(copG.nodes)
             copG.add_edges_from(edges)
                   
         depth = 1 + min(someList)
         
     elif(len(copG.nodes) > 1 and not nx.is_connected(copG)):
         someList2 = []
-        print('case3')
+#        print('case3')
         for i in (copG.subgraph(c) for c in nx.connected_components(copG)):
             someList2.append(td(i))
 
         depth = max(someList2)
         
-    print(depth)
+        
+        
+#    print(depth)
+    
     return depth
 
+def treeDecomposition(G):           
+    tree = []
+    
+    graph = {n: set(G[n]) - set([n]) for n in G}
+    
+    node_stack = []
+    
+    elim_node = treewidth.min_fill_in_heuristic(graph)
+    
+    while elim_node is not None:
+        neigh = graph[elim_node]
+        for u, v in itertools.permutations(neigh, 2):
+            if v not in graph[u]:
+                graph[u].add(v)
+                
+        node_stack.append((elim_node, neigh))
+        
+        for u in graph[elim_node]:
+            graph[u].remove(elim_node)
+            
+        del graph[elim_node]
+        elim_node = treewidth.min_fill_in_heuristic(graph)
+        
+    decomp = nx.Graph()
+    first_bag = frozenset(graph.keys())
+    tree.append(set(graph.keys()))
+    decomp.add_node(first_bag)    
+    
+    while node_stack:
+        # get node and its neighbors from the stack
+        (curr_node, nbrs) = node_stack.pop()
+
+        # find a bag all neighbors are in
+        old_bag = None
+        for bag in decomp.nodes:
+            if nbrs <= bag:
+                old_bag = bag
+                break
+
+        if old_bag is None:
+            # no old_bag was found: just connect to the first_bag
+            old_bag = first_bag
+
+        # create new node for decomposition
+        nbrs.add(curr_node)
+        new_bag = frozenset(nbrs)
 
 
-P = nx.Graph()
-P.add_nodes_from([1,2,3])
-P.add_edge(1,2)
-P.add_edge(1,3)
+        # add edge to decomposition (implicitly also adds the new node)
+        decomp.add_edge(old_bag, new_bag)
+        tree.append((nbrs))
+    
+    return tree, decomp
 
-Q = nx.path_graph(3)
 
-infile = open('alarm.dgf')
+#def td2(G):
+#    
+#    copG = G.copy()
+#    depth = 0
+#
+#    if (len(copG.nodes) == 1):
+#        depth = 1
+#        print('case1')
+##        return depth
+#        
+#    elif (len(copG.nodes) > 1):
+#        minList = []
+#        maxList = []
+#        print('case2')
+#
+#        for v in list(copG.nodes):
+#            edges = list(copG.edges(v))
+#            copG.remove_node(v)
+#            for i in (copG.subgraph(c) for c in nx.connected_components(copG)):
+#                maxList.append(td2(i))            
+#            minList.append(max(maxList))
+#            copG.add_node(v)
+#            copG.add_edges_from(edges)
+#                  
+#        depth = 1 + min(minList)
+#        
+#    print(depth)
+#    return depth
+
+
+#
+#P = nx.Graph()
+#P.add_nodes_from([1,2,3])
+#P.add_edge(1,2)
+#P.add_edge(1,3)
+#
+#Q = nx.path_graph(3)
+
+#infile = open('alarm.dgf')
+#G = nx.Graph()
+#
+#for line in infile:
+#    edge = (line.split())
+#    if edge:
+#        if edge[0] == 'e':
+#            G.add_edge(int(edge[1]), int(edge[2]))
+
+
+def original(): 
+    samples = []
+    
+    path = '/Users/uddhav/University/NewStuff/ThirdYear/COM3610/famous'
+    filenames = glob.glob(path + '/*.edge')
+    resultFile = open('results.txt', 'a+')
+    for filename in filenames:
+        cpu_time = time.time()
+    #    print(filename)
+        
+        infile = open(filename)
+        
+        G = nx.Graph()
+        
+        result = 0
+        
+        for line in infile:
+            edge = (line.split())
+            if edge:
+                if edge[0] == 'e':
+                    G.add_edge((edge[1]), (edge[2]))
+                    
+        samples.append(G)
+        
+    #    signal.signal(signal.SIGALRM, handler)
+    #    
+    #    signal.alarm(5)
+        
+        instance = os.path.basename(filename)
+        instance = instance.split('.')
+        instance = instance[0]
+        
+        
+        try:
+            signal.signal(signal.SIGALRM, handler)
+            signal.alarm(1)
+            result = td(G)
+        except TimeoutError as ex:
+            print(':(')
+         
+        cpu_time = time.time() - cpu_time    
+        resultFile.write('\n\n-----------------------------------------')
+        resultFile.write('\nCONDITIONS: \nGraph file = %s \n|V| = %i \n|E| = %i \nAlgorithm = 2^n \n' % (instance, len(G.nodes),len(G.edges)))
+        if result != 0:
+            resultFile.write('\nTree-depth = %i\n' %(result))
+            resultFile.write('CPU Time = %f\n' %(cpu_time))
+        else:
+            resultFile.write('\nTree-depth = TIMEOUT EXCEPTION\n')
+            resultFile.write('CPU Time = %f\n' %(cpu_time))
+        
+    resultFile.close()
+ 
+
+
+infile = open('Moser.edge')
+        
 G = nx.Graph()
+
+result = 0
 
 for line in infile:
     edge = (line.split())
     if edge:
         if edge[0] == 'e':
-            G.add_edge(int(edge[1]), int(edge[2]))
-            
-
-Se = list()
-X = set()
-e = 0.2
-S_size = math.floor((0.5 - e)*len(G.nodes))
-
-
-#for i in range(1, S_size):
-#    for j in G.nodes:
-#        
-#        if (len(S) <= i):
-#            S.add(j)
-#        else:
-#            Se.append(copy.deepcopy(S))
-#            S.clear()
-#            S.add(j)
-
-
-#for i in range(1, len(G.nodes)):
-#    for j in G.nodes:
-#        
-#        if (i == 1):
+            G.add_edge((edge[1]), (edge[2]))
+          
+G_Dfs = nx.dfs_tree(G)
+G_Dfs = nx.DiGraph.to_undirected(G_Dfs)
+diameter = nx.eccentricity(G_Dfs)
+td_upper = max(list(diameter.values()))
+#Se = list()
+#X = set()
+#e = 0.2
+#S_size = math.ceil((0.5 - e)*len(G.nodes))
+#
+#
+#
+#for i in G.nodes:  
+#    for j in G.neighbors(i):
+#        X.add(i)
+#        if len(X) <= S_size:
 #            X.add(j)
-#            Se.append(copy.deepcopy(X))
-#            X.clear()
-#        elif():
-#            Se.append(copy.deepcopy(S))
-#            S.clear()
-#            S.add(j)
-
-
-for i in G.nodes:  
-    for j in G.neighbors(i):
-        X.add(i)
-        if len(X) <= S_size:
-            X.add(j)
-            
-    Se.append(copy.deepcopy(X))
-    X.clear()
-
-print(Se)
-
-tdSet = list()
-
-for i in Se:
-    tdSet.append(tdAux(copy.deepcopy(i)))
-
-#for i in range(math.floor((0.25 + (1.5*e))*len(G.nodes))):
-#    for j in Se
-
-
-T = nx.minimum_spanning_tree(G)
-
-#### FINDING PROBLEMATIC VERTEX#####
-
+#            
+#    Se.append(copy.deepcopy(X))
+#    X.clear()
+#
+#print(Se)
+#
+#subGraphs = list()
+#
+#for i in Se:
+#    subGraphs.append(G.subgraph(list(i)))
+#
+#T = nx.minimum_spanning_tree(G)
+#
+##seTd = []
+##for i in subGraphs:
+##    seTd.append(td(i))
+#
+###### PROBLEMATIC VERTEX #####
+#root = random.randint(1,len(T.nodes))
+##root = list(T.nodes)[0]
+#
+#for i in T.nodes:
+#    if i == root:
+#        continue
+#    else:
+#        copyT = T.copy()
+#        sp = nx.shortest_path(copyT, source=root, target=i)
+#        print("source = %i /n target = %i", (root, i))
+#        print(sp)
+#        copyT.remove_nodes_from(sp[:-1])
+#        print(len(copyT)>S_size)
+#        print(len(sp[:-1]))
+#        
+#        if (len(copyT)>S_size and len(sp[:-1])>S_size) :
+#            problemV = i
+#            print(problemV)
+#            
+##            
---
+++ treedepthSAT.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 13:36:26 2019
+
+@author: uddhav
+"""
+import networkx as nx
+import sys
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import subprocess
+from satispy.solver import Minisat
+import time
+import pycosat
+from pathlib import Path
+
+def apex_vertex(g):
+    buff = 0
+    delete_vertices = list()
+    for u, degree in g.degree():
+        if degree == g.number_of_nodes() - 1:
+            delete_vertices.append(u)
+    g.remove_nodes_from(delete_vertices)
+    buff += len(delete_vertices)
+    nx.convert_node_labels_to_integers(g, first_label=0)
+    return g, buff
+
+def degree_one_reduction(g):
+    nodes = set()
+    for u in g.nodes():
+        deg = 0
+        for v in g.neighbors(u):
+            if g.degree(v) == 1:
+                if deg == 0:
+                    deg = 1
+                else:
+                    nodes = nodes.union({v})
+    g.remove_nodes_from(list(nodes))
+    g = nx.convert_node_labels_to_integers(g, first_label=0)
+    return g
+
+def variables(g,width):
+    nv=g.number_of_nodes()
+    p=[[[0 for i in range(width)]for j in range(nv)] for k in range(nv)]
+    nvar=1
+    for u in range(nv):
+        for v in range(u,nv):
+            for i in range(width):
+                p[u][v][i]=nvar
+                nvar+=1
+    return p,nvar-1
+
+
+def generate_encoding(g,width):
+    s,nvar=variables(g,width)
+    encoding=''
+    nclauses=0
+    nv=g.number_of_nodes()
+    for u in range(nv):
+        for v in range(u,nv):
+            encoding += '%i 0\n' % (s[u][v][width-1])
+            nclauses += 1
+            encoding += '-%i 0\n' % (s[u][v][0])
+            nclauses += 1
+    for u in range(nv):
+        for v in range(u, nv):
+            for i in range(1,width):
+                encoding += '-%i %i 0\n' % (s[u][v][i-1], s[u][v][i])
+                nclauses += 1
+    for u in range(nv):
+        for v in range(u + 1, nv):
+            for w in range(v + 1, nv):
+                for i in range(width):
+                    encoding += '-%i -%i %i 0\n' % (s[u][v][i], s[u][w][i], s[v][w][i])
+                    nclauses += 1
+                    encoding += '-%i -%i %i 0\n' % (s[u][v][i], s[v][w][i], s[u][w][i])
+                    nclauses += 1
+                    encoding += '-%i -%i %i 0\n' % (s[u][w][i], s[v][w][i], s[u][v][i])
+                    nclauses += 1
+    for u in range(nv):
+        for v in range(u + 1, nv):
+            for i in range(width):
+                    encoding += '-%i %i 0\n' % (s[u][v][i], s[u][u][i])
+                    encoding += '-%i %i 0\n' % (s[u][v][i], s[v][v][i])
+                    nclauses += 2
+    for u in range(nv):
+        for v in range(u+1,nv):
+            for i in range(1,width):
+                encoding+='-%i %i %i 0\n'%(s[u][v][i],s[u][u][i-1],s[v][v][i-1])
+                nclauses+=1
+    for e in g.edges():
+        u=min(e)
+        v=max(e)
+        for i in range(1,width):
+            encoding+='-%i %i -%i %i 0\n'%(s[u][u][i],s[u][u][i-1],s[v][v][i],s[u][v][i])
+            nclauses+=1
+            encoding+='-%i %i -%i %i 0\n'%(s[u][u][i],s[v][v][i-1],s[v][v][i],s[u][v][i])
+            nclauses+=1
+    preamble='p cnf %i %i\n'%(nvar,nclauses)
+    return preamble+encoding
+
+def decode_output(sol,g,width):
+    with open(sol, 'r') as out_file:
+        out = out_file.read()
+    out = out.split('\n')
+    if out[0] == 'UNSAT':
+        return
+    out = out[1]
+    out = out.split(' ')
+    out = list(map(int, out))
+    out.pop()
+    nv = g.number_of_nodes()
+    ne = g.number_of_edges()
+    s,nvar = variables(g, width)
+    components=list()
+    for i in range(width-1,0,-1):
+        level=list()
+        for u in range(nv):
+            ver=list()
+            for v in range(u,nv):
+                if out[s[u][v][i]-1]>0:
+                    ver.append(v)
+            do_not_add=0
+            for v in level:
+                if set(ver).issubset(set(v)):
+                    do_not_add=1
+            if do_not_add==0:
+                level.append(ver)
+        components.append(level)
+    for i in components:
+        sys.stderr.write(str(i)+'\n')
+    sys.stderr.write('\n'+"*"*10+'\n')
+    decomp=nx.DiGraph()
+    root=list()
+    level_i=list()
+    for i in range(width-1,0,-1):
+        level = list()
+        # sys.stderr.write('\n'+'*'*10+'\n')
+        for u in range(nv):
+            if out[s[u][u][i]-1]>0 and out[s[u][u][i-1]-1]<0:
+                edge_add=False
+                if i==width-1:
+                    root.append(u)
+                decomp.add_node(u,level=i)
+                # sys.stderr.write("%i "%u)
+                level.append(u)
+                if level_i!=[]:
+                    for v in level_i[len(level_i)-1]:
+                        if out[s[min(u,v)][max(u,v)][i+1]-1]>0:
+                            decomp.add_edge(v,u)
+                            edge_add=True
+                if not edge_add:
+                    if level_i!=[]:
+                        level_u=g.number_of_nodes()
+                        v_u=-1
+                        for v in decomp.nodes(data=True):
+                            if v[0]==u:
+                                continue
+                            if out[s[min(u,v[0])][max(u,v[0])][v[1]['level']]-1]>0:
+                                if level_u>v[1]['level']:
+                                    level_u=v[1]['level']
+                                    v_u=v[0]
+                        decomp.add_edge(v_u,u)
+        level_i.append(level)
+        # print level
+#    show_graph(decomp,1)
+    verify_decomp(g=g,s=decomp,width=width,root=root)
+
+
+def verify_decomp(g, s, width, root):
+    sys.stderr.write("\nValidating tree depth decomposition\n")
+    sys.stderr.flush()
+    # print g.edges()
+    for e in g.edges():
+        try:
+            nx.shortest_path(s, e[0], e[1])
+        except:
+            try:
+                nx.shortest_path(s, e[1], e[0])
+            except:
+                raise Exception("Edge %i %i not covered\n" % (e[0], e[1]))
+    for v, d in g.degree():
+        count = 0
+        if d != 1:
+            continue
+        for i in root:
+            try:
+                if len(nx.shortest_path(s, i, v)) - 1 > width:
+                    raise ValueError("depth of tree more than width\n")
+            except:
+                count += 1
+                if count == len(root):
+                    raise Exception("No root found for %i\n"%v)
+                continue
+    sys.stderr.write("Valid treedepth decomp\n")
+    sys.stderr.flush()
+
+
+def show_graph(graph, layout, nolabel=0):
+    """ show graph
+    layout 1:graphviz,
+    2:circular,
+    3:spring,
+    4:spectral,
+    5: random,
+    6: shell
+    """
+
+    m = graph.copy()
+    if layout == 9991:
+        pos = nx.nx_agraph.graphviz_layout(m)
+    elif layout == 2:
+        pos = nx.circular_layout(m)
+    elif layout == 3:
+        pos = nx.spring_layout(m)
+    elif layout == 4:
+        pos = nx.spectral_layout(m)
+    elif layout == 5:
+        pos = nx.random_layout(m)
+    elif layout == 6:
+        pos = nx.shell_layout(m)
+    if not nolabel:
+        nx.draw_networkx_edge_labels(m, pos)
+    nx.draw_networkx_labels(m, pos)
+    nx.draw_networkx_nodes(m, pos)
+    # write_dot(m, "m1.dot")
+    # os.system("dot -Tps m1.dot -o m1.ps")
+#    nx.draw(m, pos)
+    plt.show()
+    
+    
+def main():
+    instance = 'famous/Pappus.edge/'
+#    instance = 'alarm.dgf'
+#    instance = 'alarm.dgf'
+    instance = os.path.realpath(instance)
+    d = -1
+    solver = "/usr/local/Cellar/minisat/2.2.0_2/bin/minisat"
+    width = -1
+    temp = '/Users/uddhav/University/NewStuff/ThirdYear/COM3610/temp/'
+    infile = open(instance)
+    g = nx.Graph()
+
+    for line in infile:
+        edge = (line.split())
+        if edge:
+            if edge[0] == 'e':
+                g.add_edge((edge[1]), (edge[2]))
+                
+    instance = os.path.basename(instance)
+    instance = instance.split('.')
+    instance = instance[0]
+    
+    n = g.number_of_nodes()
+    m = g.number_of_edges()
+
+    g=degree_one_reduction(g=g)
+    buff = 0
+    lb = 0
+    ub = 0
+    to = False
+    g,buff=apex_vertex(g=g)
+
+    print('treedepthp2sat', instance, n, m,g.number_of_nodes(),buff)
+    if g.number_of_nodes() <= 1:
+        print(lb,ub,to)
+        exit(0)
+    encoding_time = list()
+    solving_time = list()
+    g=nx.relabel.convert_node_labels_to_integers(g,first_label=0)
+    if width == -1:
+        for i in range(g.number_of_nodes()+2,1,-1):
+            encode_time = time.time()
+            encoding = generate_encoding(g, i)
+            cnf = temp + instance + '_' + str(i) + ".cnf"
+            with open(cnf, 'w') as ofile:
+                ofile.write(encoding)
+            # with open(cnf,'r') as ifile:
+            #     s=ifile.read()
+            #     print s
+            encode_time = time.time() - encode_time
+            encoding_time.append(encode_time)
+            sol = temp + instance + '_' + str(i) + '.sol'
+#            Path(temp + instance + '_' + str(i) + '.txt').touch()
+#            anotherSol = temp + instance + '_' + str(i) + '.txt'
+            cmd = [solver, cnf, sol]
+            # print cmd
+            solving = time.time()
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, err = p.communicate()
+            rc = p.returncode
+            solving = time.time() - solving
+            solving_time.append(solving)
+            sys.stderr.write('*' * 10+'\n')
+#            print(output)
+            print(err)
+            sys.stderr.write("\n%i %i\n"%(i-1, rc))
+            if rc == 20:
+                to = True
+                if lb == 0:
+                    ub = i
+            if rc == 10:
+                if to:
+                    lb = i-2
+                decode_output(sol=sol, g=g, width=i)
+            print(i-2, lb, ub, to)
+
+#                exit(0)
+
+if __name__ == "__main__":
+    main()
